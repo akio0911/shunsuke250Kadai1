b403a84e289f8f95ee096dfddd0671e90eaa93f4
shunsuke250 <92722651+shunsuke250@users.noreply.github.com>
2022-03-24 12:55:02 +0900

    first commit

---
+++ kadai1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  kadai1
+//
+//  Created by 副山俊輔 on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
+    @IBOutlet private weak var sumButton: UIButton!
+    @IBOutlet private weak var answerLabel: UILabel!
+
+    @IBAction func sumButtonAction(_ sender: Any) {
+
+        let num0 = Int(textField1.text ?? "") ?? 0
+        let num1 = Int(textField2.text ?? "") ?? 0
+        let num2 = Int(textField3.text ?? "") ?? 0
+        let num3 = Int(textField4.text ?? "") ?? 0
+        let num4 = Int(textField5.text ?? "") ?? 0
+
+        let sum = num0 + num1 + num2 + num3 + num4
+        answerLabel.text = String(sum)
+    }
+
+}
